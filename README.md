c2b53a27a319e2a364ce8b5c0c411d9f25e3f51d
gustcomer <gustcomer@gmail.com>
2024-01-16 13:46:36 -0300

    feat: convert video to audio

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 The objective of this python project is to transcribe audios from youtube
 videos.
+
+## Instructions
+
+> pip install ffmpeg moviepy
---
+++ convert_video_to_audio.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from moviepy.editor import VideoFileClip
+
+
+def convert_video_to_audio_moviepy(video_file, output_ext="mp3"):
+    """Converts video to audio using MoviePy library
+    that uses `ffmpeg` under the hood"""
+    filename, ext = os.path.splitext(video_file)
+    print(filename)
+    print(ext)
+    clip = VideoFileClip(video_file)
+    clip.audio.write_audiofile(f"{filename}.{output_ext}")
+
+
+if __name__ == "__main__":
+    vf = sys.argv[1]
+    print(vf)
+    convert_video_to_audio_moviepy(vf)
